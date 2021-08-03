db74be56964c12af121ab54d66f0b20040b94d6e
nobel-noba <nobelmitiku@gmail.com>
2021-08-03 14:30:32 +0300

    after grid done but not fully

--- script.js
+++ script.js
@@ -1,153 +1,148 @@
-
-let containers = document.querySelectorAll('.container')
-let draggables = document.querySelectorAll('.draggable')
-const contentCreatorButtons = document.querySelectorAll('.contentCreatorButton')
+let containers = document.querySelectorAll(".container");
+let draggables = document.querySelectorAll(".draggable");
+const contentCreatorButtons = document.querySelectorAll(
+  ".contentCreatorButton"
+);
 
 let currentdragging;
 
-contentCreatorButtons.forEach(contentCreatorButton =>  {
-    contentCreatorButton.addEventListener('dragstart', () =>{
-        contentCreatorButton.classList.add('moving')
-        currentdragging = contentCreatorButton.id
-    })
-})
+contentCreatorButtons.forEach((contentCreatorButton) => {
+  contentCreatorButton.addEventListener("dragstart", () => {
+    contentCreatorButton.classList.add("moving");
+    currentdragging = contentCreatorButton.id;
+  });
+});
 
 // used to drag Elements
 
 // call this function when updating the dragging elements should be updated
-function addingEvent(){
-    draggables.forEach(draggable => {
-        draggable.addEventListener('dragstart',() => {
-            draggable.classList.add('dragging')
-            currentdragging = "notContentCreator"
-        })
-        draggable.addEventListener('dragend', () =>{
-            draggable.classList.remove('dragging')
-        })
-    })
+function addingEvent() {
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", () => {
+      draggable.classList.add("dragging");
+      currentdragging = "notContentCreator";
+    });
+    draggable.addEventListener("dragend", () => {
+      draggable.classList.remove("dragging");
+    });
+  });
 }
 
 // initial call for existing dragging elements
-addingEvent()
+addingEvent();
 
 function containerDragOver(container) {
-    container.addEventListener('dragover' , e =>{
-        e.preventDefault()
-        e.stopPropagation()
-        const afterElement = getDragAfterElement(container,e.clientY)
-        const draggable = document.querySelector('.dragging')
-        if(afterElement === null ){
-            container.appendChild(draggable)
-        }
-        else if(draggable !== null){
-            container.insertBefore(draggable,afterElement)
-        }
-        
-    })
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    if (afterElement === null) {
+      container.appendChild(draggable);
+    } else if (draggable !== null) {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
 }
-   
+
 function workingContainer(container) {
-    
-    container.addEventListener('dragenter' , () =>{
-        container.classList.add('hovered')
-    })
-    container.addEventListener('dragleave' , () =>{
-        container.classList.remove('hovered')
-    })
-    containerDragOver(container)
-    
-    container.addEventListener('drop',(e) =>{
-        e.preventDefault()
-        e.stopPropagation()
-        if(currentdragging !== "notContentCreator")
-        {
-            addContent(currentdragging+"Temp",container)
-        }
-        draggables = document.querySelectorAll('.draggable')
-        addingEvent()
-        containers = document.querySelectorAll('.container')
-    })
-    
+  container.addEventListener("dragenter", () => {
+    console.log(container);
+    container.classList.add("hovered");
+  });
+  container.addEventListener("dragleave", () => {
+    container.classList.remove("hovered");
+  });
+  containerDragOver(container);
+
+  container.addEventListener("drop", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (currentdragging !== "notContentCreator") {
+      addContent(currentdragging + "Temp", container);
+    }
+    draggables = document.querySelectorAll(".draggable");
+    addingEvent();
+    containers = document.querySelectorAll(".container");
+  });
 }
 
 // loop through the containers to place the element in any hovering container
-function setContainerEvents(){
-    containers.forEach(container => {
-        workingContainer(container)
-    })
+function setContainerEvents() {
+  containers.forEach((container) => {
+    workingContainer(container);
+  });
 }
 
-setContainerEvents()
-
+setContainerEvents();
 
 // this function will check where to put the element after in the same container
-function getDragAfterElement(container,y){
-    // avoiding the dragging class while selecting all draggable classes
-    const draggableElements =[...container.querySelectorAll('.draggable:not(.dragging)')]
-    return draggableElements.reduce((closest,child) => {
-        const box =child.getBoundingClientRect()
-        const offset = y - box.top - box.height/ 2
-        if(offset < 0 && offset > closest.offset){
-            return{offset:offset,element :child}
-        }else{
-            return closest
-        }
-    },{offset : Number.NEGATIVE_INFINITY}).element
+function getDragAfterElement(container, y) {
+  // avoiding the dragging class while selecting all draggable classes
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ];
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
 
 // Add new Content
 
-function addContent(tag,place) {
-    
-    var template = document.getElementById(tag);
-    var cloned = template.content.cloneNode(true) ;
-        for (let i = 0; i < cloned.children.length; i++) {
-            var element = cloned.children[i];
-            var rand = Math.floor(Math.random()*10000);
-            var newId = element.id+rand;
-            element.setAttribute('id',newId);
-            if(!element.classList.contains("container") && !element.classList.contains("row"))
-            {            
-                element.setAttribute('draggable','true');
-                element.classList.add("draggable")
-            }
-            
-        }
-
-
-        var R=5;
-        var C=4;
-    
-
-
-    if(tag == "gridTemp"){
-        
-    console.log("befor")
-    console.log(element)
-        for (let i = 0 ; i < R ; i++){
-            var childRow = document.createElement("div")
-            childRow.classList.add("container")
-            childRow.innerHTML = "number"
-            element.appendChild(childRow)
-        }
+function addContent(tag, place) {
+  var template = document.getElementById(tag);
+  var cloned = template.content.cloneNode(true);
+  for (let i = 0; i < cloned.children.length; i++) {
+    var element = cloned.children[i];
+    var rand = Math.floor(Math.random() * 10000);
+    var newId = element.id + rand;
+    element.setAttribute("id", newId);
+    if (
+      !element.classList.contains("container") &&
+      !element.classList.contains("row")
+    ) {
+      element.setAttribute("draggable", "true");
+      element.classList.add("draggable");
     }
-    
-    place.appendChild(cloned);
+  }
 
-    console.log("after")
-    console.log(element)
-    if(tag == "gridTemp"){
-        if(element.children.length > 1){
-            for (let i = 0 ; i < element.children.length ; i++){
-                containerDragOver(element.children[i])
-                element.children[i].classList.add(`col-${3}`)
-            }
-        }
-        containerDragOver(element)
+  var rows = 2;
+  var colomns = 2;
+
+  if (tag == "gridTemp") {
+    for (let j = 0; j < rows; j++) {
+      var childRow = document.createElement("div");
+      childRow.classList.add("container");
+      element.appendChild(childRow);
+      for (let i = 0; i < colomns; i++) {
+        var childCol = document.createElement("div");
+        childCol.classList.add("container");
+        childCol.innerHTML = "number";
+        element.children[j].appendChild(childCol);
+      }
     }
-    console.log(containers)
+  }
 
-    
-
-
+  place.appendChild(cloned);
+  if (tag == "gridTemp") {
+    if (element.children.length > 1) {
+      for (let i = 0; i < element.children.length; i++) {
+        element.children[i].classList.add("row");
+        for (let j = 0; j < element.children[i].children.length; j++) {
+          containerDragOver(element.children[i].children[j]);
+          element.children[i].children[j].classList.add(`col-${3}`);
+        }
+      }
+    }
+    containerDragOver(element);
+  }
 }
